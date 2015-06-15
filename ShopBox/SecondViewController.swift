df08b7ec890ffebd950b46c0b48cd84ff0fabfeb
yangfan <464715077@qq.com>
2015-06-15 15:19:14 +0800

    Initial Commit

---
+++ ShopBox/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ShopBox
+//
+//  Created by goyoo on 15/6/15.
+//  Copyright (c) 2015年 yfan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
