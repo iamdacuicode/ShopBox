d9a55aa5286c2f3c1ec7f28af6c7b08feac989c4
yangfan <464715077@qq.com>
2015-06-24 12:54:33 +0800

    主题色

--- ShopBox/Theme.swift
+++ ShopBox/Theme.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 struct Theme {
-    static let appColor = UIColor(red: 247/255, green: 73/255, blue: 55/255, alpha: 1)
+    static let appColor = UIColor(red: 94/255, green: 197/255, blue: 202/255, alpha: 1)//蓝绿色
+//    UIColor(red: 247/255, green: 73/255, blue: 55/255, alpha: 1)//橙红色
     
     static let lightGrayColor = UIColor(white: 0.9, alpha: 1)
     
